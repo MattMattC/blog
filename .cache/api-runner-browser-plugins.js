72321033e175587e74b9e9a4e390a9145dc96752
MattMatt <roadfen@gmail.com>
2019-05-05 13:25:18 +0200

    Initial commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/home/mattmatt/dev/gatsby-site/node_modules/gatsby-mdx/gatsby-browser.js'),
+      options: {"plugins":[],"extensions":[".mdx",".md"],"defaultLayouts":{"default":"/home/mattmatt/dev/gatsby-site/src/components/blog-post-layout.js"}},
+    },{
+      plugin: require('/home/mattmatt/dev/gatsby-site/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
